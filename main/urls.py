0ee44bac6366463963d6847939e648e9f1da47fa
Rudik Avetisyan <rudikavetisyan6@gmail.com>
2025-12-22 07:18:27 +0400

    project hw2 patrast

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path("", views.main_page, name="main_page"),
-    path("comments/<int:id>", views.comment_view, name="comment_section")
+    path("comments/<int:id>", views.comment_view, name="comment_section"),
+    path("register", views.register_view, name="register_page"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout", LogoutView.as_view(next_page="main_page"), name="logout"),
+    path("profile/", views.profile_view, name="profile"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, Profile, Comment
+from .forms import UserRegister
+from django.contrib.auth import login
 
 def main_page(request):
     posts = Post.objects.all()
@@ -10,3 +12,20 @@ def comment_view(request, id):
     comments = Comment.objects.filter(post_id = id)
     return render(request, "comments.html", {"comments": comments,
                                              "post": post})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+        return redirect("main_page")
+    
+    else:
+        form = UserRegister()
+        
+    return render(request, "register.html", {"form":form})
+
+def profile_view(request):
+    return render(request, "profile.html", {})
